144de674bf5490b0779f2e5001d1af85c76b6e94
nomadeel <z5075672@student.unsw.edu.au>
2018-04-13 23:28:13 +1000

    Refactor challenge 10

--- set2/challenge_10.cpp
+++ set2/challenge_10.cpp
@@ -23,40 +23,71 @@ std::string xor_strings(std::string &a, std::string &b) {
     return output;
 }
 
-std::string cbc_encrypt_block(std::string &input, CryptoPP::byte *key, std::string &previous_block) {
+std::string pkcs7_pad(std::string &input, unsigned int block_size) {
+    std::string copy = input;
+
+    if (block_size <= input.size())
+        return copy;
+
+    unsigned int difference = block_size - input.size();
+    
+    copy.insert(copy.end(), difference, (char) difference);
+
+    return copy;
+}
+
+std::string cbc_encrypt(std::string &input, CryptoPP::byte *key, std::string &iv) {
     CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption e;
     e.SetKey(key, 16);
 
-    std::string xored_input = xor_strings(input, previous_block);
+    unsigned int num_blocks = (input.size() / 16) + (input.size() % 16 != 0);
 
+    std::string prev_block = iv;
     std::string output;
 
-    CryptoPP::StringSource ss1(xored_input, true,
-            new CryptoPP::StreamTransformationFilter(e,
-                new CryptoPP::StringSink(output),
-                CryptoPP::BlockPaddingSchemeDef::NO_PADDING)
-    );
-
-    std::cout << output.size() << "\n";
+    for (auto i = 0U; i < num_blocks; ++i) {
+        std::string curr_block = input.substr(i*16, 16);
+        if (curr_block.size() != 16) {
+            curr_block = pkcs7_pad(curr_block, 16);
+        }
+        curr_block = xor_strings(curr_block, prev_block);
+        CryptoPP::StringSource ss1(curr_block, true,
+                new CryptoPP::StreamTransformationFilter(e,
+                    new CryptoPP::StringSink(output),
+                    CryptoPP::BlockPaddingSchemeDef::NO_PADDING)
+        );
+        prev_block = output.substr(i*16, 16);
+    }
 
     return output;
 }
 
-std::string cbc_decrypt_block(std::string &input, CryptoPP::byte *key, std::string &previous_block) {
+std::string cbc_decrypt(std::string &input, CryptoPP::byte *key, std::string &iv) {
     CryptoPP::ECB_Mode<CryptoPP::AES>::Decryption d;
     d.SetKey(key, 16);
 
+    unsigned int num_blocks = (input.size() / 16) + (input.size() % 16 != 0);
+
+    std::string prev_block = iv;
     std::string output;
+    std::string temp;
 
-    CryptoPP::StringSource ss1(input, true,
-            new CryptoPP::StreamTransformationFilter(d,
-                new CryptoPP::StringSink(output),
-                CryptoPP::BlockPaddingSchemeDef::NO_PADDING)
-    );
+    for (auto i = 0U; i < num_blocks; ++i) {
+        std::string curr_block = input.substr(i*16, 16);
+        if (curr_block.size() != 16) {
+            curr_block = pkcs7_pad(curr_block, 16);
+        }
+        CryptoPP::StringSource ss1(curr_block, true,
+                new CryptoPP::StreamTransformationFilter(d,
+                    new CryptoPP::StringSink(temp),
+                    CryptoPP::BlockPaddingSchemeDef::NO_PADDING)
+        );
+        output += xor_strings(temp, prev_block);
+        temp.clear();
+        prev_block = input.substr(i*16, 16);
+    }
 
-    std::string xored_output = xor_strings(output, previous_block);
-
-    return xored_output;
+    return output;
 }
 
 void test_cbc(void) {
@@ -71,31 +102,15 @@ void test_cbc(void) {
     std::string deciphered_ciphertext;
     deciphered_ciphertext.reserve(32);
 
-    for (auto i = 0U; i < 32; i += 16) {
-        std::string current_block = plaintext.substr(i, 16);
-        if (i == 0) {
-            ciphertext += cbc_encrypt_block(current_block, key, iv);
-        } else {
-            std::string previous_block = ciphertext.substr(i-16, 16);
-            ciphertext += cbc_encrypt_block(current_block, key, previous_block);
-        }
-    }
+    ciphertext = cbc_encrypt(plaintext, key, iv);
 
-
-    for (auto i = 0U; i < 32; i += 16) {
-        std::string current_block = ciphertext.substr(i, 16);
-        if (i == 0) {
-            deciphered_ciphertext = cbc_decrypt_block(current_block, key, iv);
-        } else {
-            std::string previous_block = ciphertext.substr(i-16, 16);
-            deciphered_ciphertext += cbc_decrypt_block(current_block, key, previous_block);
-        }
-    }
+    deciphered_ciphertext = cbc_decrypt(ciphertext, key, iv);
 
     if (plaintext == deciphered_ciphertext) {
         std::cout << "matches\n";
     } else {
         std::cout << "does not match\n";
+        std::cout << "deciphered ciphertext is:\n" << deciphered_ciphertext << "\n";
     }
 }
 
@@ -130,10 +145,10 @@ int main(void) {
     for (auto i = 0U; i < hexstring.size(); i += 16) {
         std::string current_block = hexstring.substr(i, 16);
         if (i == 0) {
-            plaintext += cbc_decrypt_block(current_block, key, iv);
+            plaintext += cbc_decrypt(current_block, key, iv);
         } else {
             std::string previous_block = hexstring.substr(i-16, 16);
-            plaintext += cbc_decrypt_block(current_block, key, previous_block);
+            plaintext += cbc_decrypt(current_block, key, previous_block);
         }
     }
 
